106dfddba5e0fbd0c4b97db2b2e17bc09b4daf6a
yuval.maliniak <yuvalmali5@gmail.com>
2024-05-25 13:58:13 +0300

    Fixed all loan ops

--- loan_controllers.py
+++ loan_controllers.py
@@ -11,87 +11,79 @@ class LoanOperations:
         self.id = 1  # Books ID
 
     def create_loan(self, data):
-        # Check validity: Missing fields, existing ID, existing ISBN
-        if 'title' not in data or 'ISBN' not in data or 'genre' not in data:
-            error_message = "Missing fields"
-            return error_message, 422
-        if None in (data['title'], data['ISBN'], data['genre']):
-            error_message = "Missing fields"
-            return error_message, 422
-        for book in self.books_data:
-            if book['ISBN'] == data['ISBN']:
-                error_message = "There already exists a book in /book with this ISBN number"
-                return error_message, 422
-                
         # Extract required data from the POST request
-        genre = data['genre']
+        memberName = data['memberName']
         isbn = data['ISBN']
-        if genre not in genre_values:
-            error_message = "genre is not one of the accepted values"
+        loanDate = data['loanDate']
+        book_json = {
+            'ISBN': isbn
+        }
+        # Check validity: Missing fields, Wrong ISBN, Too much loans
+        book_data, error_code = self.get_from_books("GET", book_json)
+        if error_code != 200:
+            error_message = f"Sorry {memberName},  the ISBN is not in the library"
             return error_message, 422
-        # Enter data into the new book
-        new_book = {
-            'title': data['title'],
-            'ISBN': data['ISBN'],
-            'genre': data['genre'],
-            'id': str(self.id)
+        num_of_loans = 0
+        for loan in self.loan_data:
+            if loan['ISBN'] == isbn:
+                error_message = f"Sorry {memberName}, The book is already on loan"
+                return error_message, 422
+            if loan['memberName'] == memberName:
+                num_of_loans += 1
+        if num_of_loans >= 2:
+            error_message = f"Sorry {memberName},  You already have 2 or more books on loan"
+            return error_message, 422
+        # Enter data into the new loan
+
+        # Get today's date
+        today_date = datetime.date.today()
+        # Format the date as a string in 'YYYY-MM-DD' format
+        formatted_date = today_date.strftime('%Y-%m-%d')
+        new_loan = {
+            'title': book_data['title'],
+            'ISBN': isbn,
+            'bookID': book_data['id'],
+            'loanID': str(self.id),
+            'memberName': memberName,
+            'loanDate': str(formatted_date)
         }
         self.id+=1
-        # Handle Google API part
-        google_books_data, error_code = self.googleapi(isbn)
-        # Check if the call to Google went smooth
-        if error_code == 200:
-            new_book.update(google_books_data)
-            length = len(new_book['authors'])
-            # Make sure that multiple authors are being added by 'and' between their names.
-            author = ""
-            for i, value in enumerate(new_book['authors']):
-                if i == (length-1):
-                    author += value
-                else:
-                    author += value + " and "
-            new_book['authors'] = author
+        self.loan_data.append(new_loan)
+        return new_loan['loanID'], 201
+
+    # Function to get all loans
+    def get_all_loans(self, query_params=None):
+        returned_loans = []
+        if query_params:
+            books, error_code = self.get_from_books("GET", query_params)
+            if error_code == 200:
+                book_ids = [book['id'] for book in books]
+                returned_loans = [loan for loan in self.loan_data if loan['id'] in book_ids]
         else:
-            return google_books_data, error_code
-        self.books_data.append(new_book)
-        self.ratings.create_rating_data(self.id-1, new_book['title'])
-        return new_book['id'], 201
+            returned_loans = self.loan_data
+        return returned_loans, 200
 
+    # Function to get a single loan by ID
 
-    # Function to get all books
-    def get_all_loans(self,query_dict=None):
-        returned_books = []
-        if query_dict:
-            # Handle all query params. The check for validity is being done in main
-            for book in self.books_data:
-                if all(book.get(query) == value for query, value in query_dict.items()):
-                    returned_books.append(book)
-        else:
-            returned_books = self.books_data
-        return returned_books, 200
-
+    def get_loan_by_id(self,loan_id):
+        for loan in self.loan_data:
+            if loan['id'] == str(loan_id):
+                return loan, 200
+        return None, 404
     # Function to get a single book by ID
 
-    def get_loan_by_id(self,book_id):
-        for book in self.books_data:
-            if book['id'] == str(book_id):
-                return book, 200
-        return None, 404
-
-
     # Function to delete a book by ID
-    def delete_loan(self, book_id):
-        for i, book in enumerate(self.books_data):
-            if book['id'] == str(book_id):
-                del self.books_data[i]
-                self.ratings.deleteratings(book['id'])
-                return book_id, 200
+    def delete_loan(self, loan_id):
+        for i, loan in enumerate(self.loan_data):
+            if loan['loanID'] == str(loan_id):
+                del self.loan_data[i]
+                return loan['loanID'], 200
         # No book found, return 404 error code
         return None, 404
     def get_from_books(self, method, params):
         # Define the base URL
-        url = "http://books-services:8090/books"
-        
+        #url = "http://books-services:8090/books"
+        url = "https://localhost:8000/books"
         # Check if the method is GET
         if method.upper() == "GET":
             try:
@@ -102,12 +94,12 @@ class LoanOperations:
                 response.raise_for_status()
                 
                 # Return the JSON response
-                return response.json()
+                return response.json(), 200
             except requests.exceptions.RequestException as e:
                 # Handle exceptions (e.g., network problems, invalid responses)
                 print(f"An error occurred: {e}")
-                return None
+                return None, 404
         else:
             print("Invalid method. Only 'GET' is supported.")
-            return None
+            return None, 404
         
--- loan_main.py
+++ loan_main.py
@@ -37,107 +37,35 @@ class Loans(Resource):
             return {'error': 'Invalid JSON format'}, 415  # Return JSON with error message and HTTP status code 415 (Bad Request)
 
     def get(self):
-        genre_values = ['Fiction', 'Children', 'Biography', 'Science', 'Science Fiction', 'Fantasy', 'Other']
         query_params = request.args
-        query_dictionary = query_params.to_dict()
         if query_params:
-            if 'genre' in query_dictionary:
-                if query_dictionary['genre'] not in genre_values:
-                    return {"error": "genre is not one of the accepted values"}, 422
-            return controller.get_all_books(query_dict=query_dictionary)
+            return controller.get_all_loans(query_params=query_params)
         else:
             # Handle the case where there are no query parameters
             # Call a method in the controller to get all books
-            books, error_code = controller.get_all_books(query_dict=None)
+            books, error_code = controller.get_all_loans(query_params=None)
             return books, error_code
 
-
-class BookID(Resource):
-    def get(self,bookid):
-        books, error_code = controller.get_book_by_id(bookid)
-        if books is not None:
-            return books, error_code
+class loanID(Resource):
+    def get(self,loanID):
+        loan, error_code = controller.get_loan_by_id(loanID)
+        if loan is not None:
+            return loan, error_code
         else:
-            return {'error': 'Book not found'}, 404
+            return {'error': 'Loan not found'}, 404
 
-    def put(self,bookid):
-        # Logic to update a specific book using the book_id
-        # Check if the request is json. If not, return error 415 with relevant error message
-        if not request.is_json or request.headers['Content-Type'] != 'application/json':
-            return {"error": "Unsupported media type"}, 415
-        data = request.get_json()
-        output, error_code = controller.update_book(bookid, data)
+    def delete(self,loanID):
+        loan, error_code = controller.delete_loan(loanID)
         if error_code == 200:
-            return {'id': output}, 200
+            return {"loanID": loan}, 200
         else:
-            return {'error': output},  error_code
+            return {"error": "Loan not found"}, error_code
 
-    def delete(self,bookid):
-        idofbook, error_code = controller.delete_book(bookid)
-        if error_code == 200:
-            return {"id": idofbook}, 200
-        else:
-            return {"error": "Book not found"}, error_code
+api.add_resource(Loans, '/loans')
+api.add_resource(loanID, '/loans/<int:loanID>')
 
-
-class Ratings(Resource):
-    def get(self):
-        # Logic to fetch all ratings
-        # Call the relevant function: one for GET with query, and one for GET without query.
-        # If the query is not "id", return error code 422
-        query_params = request.args
-        if query_params == {}:
-            return controller.ratings.get_all_ratings()
-        elif "id" in query_params.keys():
-            return controller.ratings.get_rating_by_id(query_params["id"])
-        else:
-            return {"error": "Unprocessable Content"}, 422
-
-
-class SingleRating(Resource):
-    def get(self,bookid):
-        args = request.get_json()
-        try:
-            output, error_code = controller.getRating().get_rating(bookid)
-            return {"id": output}, error_code
-        except json.JSONDecodeError as e:
-            return {'error': 'Invalid JSON format'}, 415  # Return JSON with error message and HTTP status code 415 (Bad Request)
-        # Logic to fetch a specific rating using the rating_id
-
-
-class RatingValues(Resource):
-    def post(self, bookid):
-        args = request.get_json()
-        try:
-            value = args["value"]
-            output, error_code = controller.getRating().add_values_to_ratings(bookid, value)
-            if error_code == 200:
-                return {"avg": output}, error_code
-            else:
-                return {"error": output}, error_code
-        except json.JSONDecodeError as e:
-            return {'error': 'Invalid JSON format'}, 415  # Return JSON with error message and HTTP status code 415 (Bad Request)
-
-
-class Top(Resource):
-    def get(self):
-        # Logic to fetch top-rated books or ratings
-        #args = request.get_json()
-        try:
-            top_sorted_books = controller.getRating().top()
-            return top_sorted_books, 200
-        except json.JSONDecodeError as e:
-            return {'error': 'Invalid JSON format'}, 415  # Return JSON with error message and HTTP status code 415 (Bad Request)
-
-api.add_resource(Books, '/books')
-api.add_resource(BookID, '/books/<int:bookid>')
-api.add_resource(Ratings, '/ratings')
-api.add_resource(SingleRating, '/ratings/<int:bookid>')
-api.add_resource(RatingValues, '/ratings/<int:bookid>/values')
-api.add_resource(Top, '/top')
-
-if __name__ == '__main__':
+if __name__ == '__loan_main__':
     # Define the API routes using Flask
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
 
 
